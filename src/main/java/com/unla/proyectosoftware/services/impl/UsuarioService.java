5828b8371ef2010e1dc58710e7e26480b47d9eb8
MarianoDigangi1 <marianodigangi1@gmail.com>
2021-05-29 14:54:41 -0300

    prueba

--- src/main/java/com/unla/proyectosoftware/services/impl/UsuarioService.java
+++ src/main/java/com/unla/proyectosoftware/services/impl/UsuarioService.java
@@ -3,11 +3,7 @@ package com.unla.proyectosoftware.services.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-<<<<<<< HEAD
-import com.unla.proyectosoftware.converters.UsuarioConverter;
-=======
 import com.unla.proyectosoftware.converter.UsuarioConverter;
->>>>>>> 7fd0bf7b590021ae94c58e0b293ef80c690d3235
 import com.unla.proyectosoftware.models.UsuarioModel;
 import com.unla.proyectosoftware.repository.IUsuarioRepository;
 import com.unla.proyectosoftware.services.IUsuarioService;
